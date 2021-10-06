5db3ce7d599bcd625bfe5530f093c8d1c4a9f119
zicokiller <mrichet.cb@gmail.com>
2021-10-06 11:46:01 +0200

    styledtest: adding components and styled folders, create Container and Header.Styled.js

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import { StyledHeader } from "./styled/Header.styled"
+export default function Header() {
+    return (
+        <StyledHeader>
+            <h1>Hubble</h1>
+        </StyledHeader>
+    )
+}

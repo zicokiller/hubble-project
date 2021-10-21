ab486c9061594a61bb286186ee7ac221b7b3b396
zicokiller <mrichet.cb@gmail.com>
2021-10-21 15:50:23 +0200

    final commit Hubble project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { ThemeProvider } from 'styled-components';
-import Header from './components/Header';
-import {Container} from './components/styled/Container.styled';
-import GlobalStyles from './components/styled/Global';
+import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Card from './components/Card'
+import { Container } from './components/styles/Container.styled'
+import GlobalStyles from './components/styles/Global'
+import content from './content'
 
 const theme = {
   colors: {
-    header: "#ebfbff",
-    body: "#eff",
-    footer: "#003333"
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333',
   },
+  mobile: '768px',
 }
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <>
-    <GlobalStyles />
-    <Header />
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
-    </>
+      <>
+        <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -1,10 +1,12 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap');
+
   * {
     box-sizing: border-box;
   }
+
   body {
     background: ${({ theme }) => theme.colors.body};
     color: hsl(192, 100%, 9%);
@@ -12,13 +14,15 @@ const GlobalStyles = createGlobalStyle`
     font-size: 1.15em;
     margin: 0;
   }
+
   p {
     opacity: 0.6;
     line-height: 1.5;
   }
+
   img {
     max-width: 100%;
 }
 `
 
-export default GlobalStyles;
+export default GlobalStyles
